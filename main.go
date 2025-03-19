06b36c0dbd389cc6daa7da954b5b44c359c403f7
GitHub Action <action@github.com>
2025-03-19 07:14:50 +0000

    enhance file reading logic to handle line breaks and improve logging

--- main.go
+++ main.go
@@ -4,23 +4,52 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+	
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	defer file.Close()
-	buffer := make([]byte, 8)
+	
+	currentLine := ""
 	for {
+		
+		buffer := make([]byte, 8)
+		
 		bytesRead, err := file.Read(buffer)
-		if err == io.EOF {
-			break
-		}
+		
 		if err != nil {
 			log.Fatal(err)
+			if err == io.EOF {
+				break
+			}
 		}
-		log.Printf("read: %s\n", buffer[:bytesRead])	
+		
+		data := string(buffer[:bytesRead])
+        
+        parts := strings.Split(data, "\n")
+        
+        for i := range parts {
+            if i < len(parts)-1 {
+                currentLine += parts[i]
+                log.Printf("read: %s", currentLine)
+                currentLine = ""
+            } else if strings.HasSuffix(data, "\n") {
+                currentLine += parts[i]	
+                log.Printf("read: %s", currentLine)
+                currentLine = ""
+            } else {
+                currentLine += parts[i]
+            }
+        }	
 	}
+
+	if currentLine != "" {
+        log.Printf("read: %s", currentLine)
+    }
 }
