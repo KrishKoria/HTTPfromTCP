5c2a0429e807286ded63fffac57ea96c1139618f
GitHub Action <action@github.com>
2025-03-19 07:34:58 +0000

    implement TCP server to read lines from connections and improve logging

--- main.go
+++ main.go
@@ -1,55 +1,79 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
 )
 
 func main() {
-	
-	file, err := os.Open("messages.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	defer file.Close()
-	
-	currentLine := ""
-	for {
-		
-		buffer := make([]byte, 8)
-		
-		bytesRead, err := file.Read(buffer)
-		
-		if err != nil {
-			log.Fatal(err)
-			if err == io.EOF {
-				break
-			}
-		}
-		
-		data := string(buffer[:bytesRead])
-        
-        parts := strings.Split(data, "\n")
-        
-        for i := range parts {
-            if i < len(parts)-1 {
-                currentLine += parts[i]
-                log.Printf("read: %s", currentLine)
-                currentLine = ""
-            } else if strings.HasSuffix(data, "\n") {
-                currentLine += parts[i]	
-                log.Printf("read: %s", currentLine)
-                currentLine = ""
-            } else {
-                currentLine += parts[i]
-            }
-        }	
-	}
-
-	if currentLine != "" {
-        log.Printf("read: %s", currentLine)
+    listener, err := net.Listen("tcp", ":42069")
+    if err != nil {
+        log.Fatal(err)
     }
+    defer listener.Close()
+    
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Printf("Error accepting connection: %v", err)
+            continue
+        }
+        
+        log.Printf("Connection accepted")
+        
+        lines := getLinesChannel(conn)
+        for line := range lines {
+            fmt.Println(line)
+        }
+        
+        log.Printf("Connection closed")
+    }
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+    lines := make(chan string)
+    go func() {
+        defer f.Close()
+        
+        buffer := make([]byte, 8)
+        currentLine := ""
+        
+        for {
+            bytesRead, err := f.Read(buffer)
+            if err == io.EOF {
+                break
+            }
+            if err != nil {
+                log.Fatal(err)
+            }
+            
+            data := string(buffer[:bytesRead])
+            parts := strings.Split(data, "\n")
+            
+            for i := 0; i < len(parts); i++ {
+                if i < len(parts)-1 {
+                    currentLine += parts[i]
+                    lines <- currentLine
+                    currentLine = ""
+                } else if strings.HasSuffix(data, "\n") {
+                    currentLine += parts[i]
+                    lines <- currentLine
+                    currentLine = ""
+                } else {
+                    currentLine += parts[i]
+                }
+            }
+        }
+        
+        if currentLine != "" {
+            lines <- currentLine
+        }
+        
+        close(lines)
+    }()
+    
+    return lines
 }
