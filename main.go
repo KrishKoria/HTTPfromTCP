530ad1f9c3f945bf7801ca3bce3985fee19fa3f7
GitHub Action <action@github.com>
2025-03-19 07:05:20 +0000

    add file reading functionality to main function

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	buffer := make([]byte, 8)
+	for {
+		bytesRead, err := file.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("read: %s\n", buffer[:bytesRead])	
+	}
 }
